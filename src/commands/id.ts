8d2c24fa9c5c6e358c221a4814ab8f44804bc219
Woomymy <github@woomy.ovh>
2021-09-28 15:45:38 +0200

    Auth: Add command

--- src/commands/id.ts
+++ src/commands/id.ts
@@ -4,7 +4,11 @@ import {
 } from "@federation-interservices-d-informatique/fiibot-common";
 import { argon2i } from "argon2-ffi";
 import { randomBytes } from "crypto";
-import { ApplicationCommandOptionChoice, CommandInteraction } from "discord.js";
+import {
+    ApplicationCommandOptionChoice,
+    CommandInteraction,
+    WebhookClient
+} from "discord.js";
 import { CODENAMES, SERVERS } from "../utils/constants.js";
 import { transformUserName } from "../utils/transformUserNames.js";
 const getServersChoices = (): ApplicationCommandOptionChoice[] => {
@@ -41,6 +45,25 @@ export default class IdCommand extends Command {
                             description: "Le serveur auquel sera lié l'ID",
                             required: true,
                             choices: getServersChoices()
+                        }
+                    ]
+                },
+                {
+                    type: "SUB_COMMAND",
+                    name: "auth",
+                    description: "Vérifier que son ID FII est valide",
+                    options: [
+                        {
+                            type: "STRING",
+                            name: "username",
+                            description: "Votre nom d'utilisateur",
+                            required: true
+                        },
+                        {
+                            type: "STRING",
+                            name: "id",
+                            description: "Votre ID",
+                            required: true
                         }
                     ]
                 }
@@ -102,7 +125,7 @@ export default class IdCommand extends Command {
             );
             const id = `FII-${CODENAMES[server]}-${index}-${random}-FII`;
             const salt = randomBytes(32);
-            const hashedID = argon2i.hash(id, salt);
+            const hashedID = await argon2i.hash(id, salt);
             await this.client.dbclient.set(`id-${finalUserName}`, hashedID);
 
             createdUsers.push(interaction.user.id);
@@ -111,6 +134,64 @@ export default class IdCommand extends Command {
                 ephemeral: true,
                 content: `Votre ID sera ${id}. Notez le **en lieu sur**. Il sera automatiquement effacé lors du prochain redémarrage de Discord`
             });
+        } else if (interaction.options.getSubcommand() === "auth") {
+            const username = transformUserName(
+                interaction.options.getString("username")
+            );
+            const inputID = interaction.options.getString("id");
+
+            if (!(await this.client.dbclient.has(`id-${username}`))) {
+                await interaction.reply(
+                    `Aucun id n'existe avec ne nom d'utilisateur ${username}`
+                );
+                return;
+            }
+
+            const hashedID = await this.client.dbclient.get<string>(
+                `id-${username}`
+            );
+
+            if (await argon2i.verify(hashedID, inputID)) {
+                interaction.reply({
+                    ephemeral: true,
+                    embeds: [
+                        {
+                            title: "Authentification réussie!",
+                            color: "GREEN"
+                        }
+                    ]
+                });
+                try {
+                    const authWhClient = new WebhookClient({
+                        id: process.env.AUTH_HOOK_ID,
+                        token: process.env.AUTH_HOOK_TOKEN
+                    });
+
+                    authWhClient.send({
+                        embeds: [
+                            {
+                                title: `Authentification réussie pour ${interaction.user.tag} (${interaction.user.id})`,
+                                color: "GREEN",
+                                fields: [
+                                    {
+                                        name: "Nom d'utilisteur",
+                                        value: username
+                                    }
+                                ]
+                            }
+                        ]
+                    });
+                } catch (e) {
+                    await interaction.followUp(
+                        "Imossible de communiquer la réussite au C.A de la FII!"
+                    );
+                }
+            } else {
+                await interaction.reply({
+                    ephemeral: true,
+                    content: "Combinaison ID/Nom d'utilisateur incorrect"
+                });
+            }
         }
     }
 }
