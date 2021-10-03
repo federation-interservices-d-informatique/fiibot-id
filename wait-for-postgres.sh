d05c6d05383159cee8fe398ba4f28fe4087428c4
Woomymy <github@woomy.ovh>
2021-10-03 15:06:30 +0200

    Wait: Just sleep

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -2,8 +2,6 @@
 
 # Wait for postgresql before starting the bot
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "${DB_HOST}" -U "${POSTGRES_USER}" -c '\q'; do
-  sleep 1
-done
-  
+sleep 10
+
 exec "$@"
