7628c18de48cb59038ac250e743e5d8b41b32707
Woomy <woomy@woomy.be>
2021-10-03 15:00:37 +0200

    Wait: Use SH

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Wait for postgresql before starting the bot
 
-set -euo pipefail
-  
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "${DB_HOST}" -U "${POSTGRES_USER}" -c '\q'; do
   sleep 1
 done
