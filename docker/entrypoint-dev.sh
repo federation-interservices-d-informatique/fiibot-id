7daa356f2310694a9f5b2a2747d54a8862b36346
Woomy <woomy@woomy.be>
2022-05-03 19:45:39 +0200

    Docker: Remove dependency to postgres and rewrite postgres wait script in JS

--- docker/entrypoint-dev.sh
+++ docker/entrypoint-dev.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-until PGPASSWORD="${POSTGRES_PASSWORD}" psql -h "${DB_HOST}" -U "${POSTGRES_USER}" -c '\q' -d "${POSTGRES_DB}" 2>/dev/null
-do
-    echo "PostgreSQL is starting... Waiting"
-    sleep 1
-done
-
+NODE_PATH="${PWD}/node_modules" node "${PWD}/wait-for-postgres.js"
+echo "Starting bot!"
 nodemon --watch 'src/*' --ext 'ts' --exec 'npm run start-build'
