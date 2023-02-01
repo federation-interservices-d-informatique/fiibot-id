100dc49c4f48a3c1264970efa7b020c3925bf0f6
Woomy <woomy@woomy.be>
2023-02-01 18:36:42 +0100

    refactor: Initial commit for fiibot-common@v4 and djs v14
    * Upgrade all dependencies including fiibot-common to v4 and discord.js
      to v14
    
    * Enable TypeScript strict mode and extend common fii tsconfig
    
    Signed-off-by: Woomymy <woomy@woomy.be>

--- src/utils/transformUserNames.ts
+++ src/utils/transformUserNames.ts
@@ -1,3 +1,8 @@
+/**
+ * Removes all spaces from an username and replace them by _
+ * @param username The username
+ * @returns {string} Clean username
+ */
 export function transformUserName(username: string): string {
     return username.replace(/ +/g, "_");
 }
