630c3f65d9f6360ec6b076c48e0e8c536973519c
Woomymy <woomy@woomy.be>
2023-02-01 18:37:39 +0100

    docker: Move to tsc-watch
    Signed-off-by: Woomymy <woomy@woomy.be>

--- docker/entrypoint-dev.sh
+++ docker/entrypoint-dev.sh
@@ -3,4 +3,5 @@ set -e
 
 NODE_PATH="${PWD}/node_modules" node "${PWD}/wait-for-postgres.js"
 echo "Starting bot!"
-nodemon --watch 'src/*' --ext 'ts' --exec 'npm run start-build'
+tsc-watch --onSuccess "npm run start-build"
+
